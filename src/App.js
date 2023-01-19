8c8b1d1ef9bec1c25b24c9b6a84d1b0bb9acffa5
kennedy <kenneyrit@gmail.com>
2023-01-19 12:34:22 +0300

    Updated the components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Transaction from './components/transaction';
+import SearchInput from './components/SearchInput';
+import NewTransaction from './components/NewTransaction';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center className="App">
+    <SearchInput/>
+    <NewTransaction/>
+    <Transaction/>
+    
+    </center>
   );
 }
 
