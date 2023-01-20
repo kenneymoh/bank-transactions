87d3b82d4a3580432be27394ca7502ea51efa0f4
kennedy <kenneyrit@gmail.com>
2023-01-20 12:12:12 +0300

    corrected the db.json link

--- src/components/transaction.js
+++ src/components/transaction.js
@@ -7,7 +7,7 @@ function Transaction (){
     console.log(transactions)
 
     useEffect (()=>{
-      fetch ('https://github.com/MakenaG/Flatiron-Bank/blob/main/db.json')
+      fetch ('https://github.com/kenneymoh/bank-transactions/blob/main/src/db.json')
       .then(response => response.json())
       .then((data) => 
         setTransactions(data.transactions))
